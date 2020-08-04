e61586f136964744af2a9a03a08edac6da20137c
Samson Nguyen <samson.nguyen@mavs.uta.edu>
2020-08-03 21:56:16 -0500

    Started Problem 49, Prime permutations

---
+++ Python/Problem_49.py
@@ -0,0 +1,27 @@
+# Prime permutations
+#
+# The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways:
+#   (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+#
+# There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property,
+#   but there is one other 4-digit increasing sequence.
+#
+# What 12-digit number do you form by concatenating the three terms in this sequence?
+
+from Problem_26 import is_prime
+
+
+primes = {}
+for n in range(1234, 9877):
+    if is_prime(n):
+        s = ''
+        p = sorted(str(n))
+        for d in p:
+            s += d
+        if s in primes.keys():
+            primes[s].append(n)
+        else:
+            primes[s] = [n]
+print(primes)
+
+
