fc3e45c72f0de4aa17e840b45bc1bfc683b41d4b
Samson Nguyen <samson.nguyen@mavs.uta.edu>
2020-08-24 13:10:10 -0500

    Problem 49, Prime permutations

--- Python/Problem_49.py
+++ Python/Problem_49.py
@@ -10,18 +10,18 @@
 
 from Problem_26 import is_prime
 
-
 primes = {}
 for n in range(1234, 9877):
     if is_prime(n):
-        s = ''
-        p = sorted(str(n))
-        for d in p:
-            s += d
-        if s in primes.keys():
-            primes[s].append(n)
+        digits = ''.join(sorted(str(n)))
+        if digits in primes.keys():
+            primes[digits].append(n)
         else:
-            primes[s] = [n]
-print(primes)
-
-
+            primes[digits] = [n]
+print(len(primes))
+primes = dict(filter(lambda x: len(x[1]) > 2, primes.items()))
+print(len(primes), primes)
+for permutations in primes.values():
+    for p1 in range(len(permutations) - 2):
+        if permutations[p1] + 3330 in permutations and permutations[p1] + 6660 in permutations:
+            print(str(permutations[p1]), str(permutations[p1] + 3330), str(permutations[p1] + 6660))
